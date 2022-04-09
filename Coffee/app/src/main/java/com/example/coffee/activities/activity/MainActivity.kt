2fb206ebb114de489ee31ab6e5e6c3aed6f2db2e
HipoHipp <3sam1girl@naver.com>
2022-04-09 18:34:52 +0900

    hw_MyRecipeRecyclerView
    - MyRecipeRecyclerView 가 제대로 나오지 않던 부분 수정
    - nullPointer가 발생했던 부분을 기반으로 오류 발생 원인을 주석으로 정리(MainActivity 참고)

--- Coffee/app/src/main/java/com/example/coffee/activities/activity/MainActivity.kt
+++ Coffee/app/src/main/java/com/example/coffee/activities/activity/MainActivity.kt
@@ -16,14 +16,17 @@ private val fragmentHistory by lazy { HistoryFragment() }
 private val fragmentMyRecipe by lazy { MyRecipeFragment() }
 private val fragmentSetting by lazy { SettingFragment() }
 
-
 class MainActivity:BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_main)
+
+        //초기화 작업만 해주세요
     }
 
     override fun setUp() {
+        //바인딩 설정(초기설정) 이후의 실행 코드는 이곳에 작업 해주세요
+
         binding.bottomNavigation.run {
             setOnNavigationItemSelectedListener {
                 when(it.itemId) {
